bb499b6868ab1becd03116fcf42af831cb6e10f6
Chaitanya <ChaitanyaKadiyala005@gmail.com>
2025-10-05 12:13:20 +0530

    TwoSum Problem

---
+++ TwoSum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    public class TwoSum
+    {
+
+        public int[] TwoSumBF(int[] nums, int target)
+        {
+            // Brute Force:
+
+            for (int i = 0; i < nums.Length; i++) {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return [];
+        }
+
+        public int[] TwoSumOptimal(int[] nums, int target)
+        {
+            // Brute Force:
+
+            Dictionary<int,int> dict = new Dictionary<int,int>();
+
+            for (int i= 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+
+                if (dict.ContainsKey(diff))
+                {
+                    return new int[] { dict[diff], i };
+                }
+
+                if (dict.ContainsKey(nums[i]))
+                {
+                    dict[nums[i]] = i;
+                }
+                else
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+            return [];
+        }
+    }
+}
